f44cac8e5ce9525c07c6262238ac62bf5e4488e5
DavidCarvallo <carvallodavid78@gmail.com>
2022-09-27 01:41:04 -0300

    componentes, imagenes y animaciones

--- Desktop/Angular-portfolio/src/app/app.module.ts
+++ Desktop/Angular-portfolio/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { EducationComponent } from './componentes/education/education.component';
+import { ExperienceComponent } from './componentes/experience/experience.component';
+import { ProjectsComponent } from './componentes/projects/projects.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ContactComponent } from './componentes/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
